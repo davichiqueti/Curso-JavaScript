2001a0044ed6ceb14322aee331a45b18d19eda60
davichiqueti <davichiqueti@gmail.com>
2023-09-21 23:39:42 -0300

    Update Chapter 4 Exercises

--- Módulo 4 - Exercícios/ex1/script.js
+++ Módulo 4 - Exercícios/ex1/script.js
@@ -5,15 +5,20 @@ function load(){
     var hour_now = time_date_now.getHours()
     var minutes_now = time_date_now.getMinutes()
     
-    message.innerHTML = `Agora são ${hour_now} horas. `
-    if (6 < hour_now && hour_now < 12){
+    message.innerHTML = `${hour_now} hora(s) e ${minutes_now} minuto(s). `
+
+    if (hour_now >= 6 && hour_now < 12){
         // Casos em que a página é aberta de manhã
         image.src = "images/morning.png"
-    } else if (12 < hour_now && hour_now < 19) {
+        document.body.style.background = '#ecb653'
+    } else if (hour_now >= 12 && hour_now < 19) {
         // Casos em que a página é aberta a tarde
         image.src = "images/afternoon.png"
+        document.body.style.background = '#F88379'
     } else {
         // Casos em que a página é aberta a noite
         image.src = "images/night.png"
+        document.body.style.background = '#151a33'
     }
 }
+
